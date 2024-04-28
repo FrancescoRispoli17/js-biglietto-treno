44e9d3944c699d1023dfa84894aef1fe19066829
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-04-28 19:15:04 +0200

    [UPD]second IF and 2 decimal

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,10 +1,12 @@
 const Km= prompt('inserisci i kilometri');
 const eta= prompt('inserisci età');
-let prezzo=Km*0.21;
-if(eta < 18)
-{
-    prezzo=prezzo - prezzo / 5;
+let prezzo=(Km*0.21).toFixed(2);
+if(eta < 18){
+    prezzo=(prezzo - prezzo / 5).toFixed(2);
+}
+if(eta>=65){
+    prezzo=(prezzo - prezzo / 5 * 2).toFixed(2);
 }
 
 const prezzo_finale=document.getElementById('prezzo');
-prezzo_finale.innerHTML=prezzo;
+prezzo_finale.innerHTML=prezzo+'€';
